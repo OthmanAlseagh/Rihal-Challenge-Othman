fe5a204486b79c1831876c33bd61927536312345
Othman <assaigh.2013@gmail.com>
2021-11-17 16:07:55 +0300

    update master page

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -7,17 +7,26 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Rihal Challenge</title>
     <link rel="stylesheet" href='css/bootstrap.css'>
-    <link rel="stylesheet" href='css1/Student.css'>
+    <link rel="stylesheet" href='css/master.css'>
     <script src="js/html5shiv.min.js"></script>
     <script src="js/responod.min.js"></script>
     <meta name="keywords" content="website keywords, website keywords"/>
     <script type="text/javascript" src="js/modernizr-1.5.min.js"></script>
 </head>
-<body>
+<body >
 <header>
 </header>
-<div style="padding-right: 400px">
+<div style=" padding-right: 400px">
+    <nav>
+        <div class="theme-switch-wrapper">
+            <label class="theme-switch" for="checkbox">
+                <input type="checkbox" id="checkbox" />
+                <div class="slider round"></div>
+            </label>
+            <em>Enable Dark Mode!</em>
+        </div>
 
+    </nav>
     <ul class="nav nav-pills">
         <li>
         <li><a class="btn btn-primary" href="{{ url('/count-level') }}" role="button">Count Of Students Per Level</a>
@@ -39,13 +48,28 @@
 <script type="text/javascript" src="js/jquery.sooperfish.js"></script>
 <script type="text/javascript" src="js/jquery.kwicks-1.5.1.js"></script>
 <script type="text/javascript">
-    $(document).ready(function () {
-        $('#images').kwicks({
-            max: 600,
-            spacing: 2
-        });
-        $('ul.sf-menu').sooperfish();
-    });
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+
+        if (currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+        }
+    }
+
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+        else {        document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }
+    }
+
+    toggleSwitch.addEventListener('change', switchTheme, false);
 </script>
 </body>
 </html>
