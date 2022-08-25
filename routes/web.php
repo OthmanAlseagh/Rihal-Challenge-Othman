aa1c075afc1d6df5fecf276bf24642666fa2473e
Othman <assaigh.2013@gmail.com>
2022-08-25 16:48:09 +0300

    update mechanism

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Country;
 use App\Models\Level;
 use App\Models\Student;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,18 +23,34 @@ Route::get('/', function () {
 });
 
 Route::get('student-date', function () {
-    $collection = Student::get()->pluck('date_of_birth')->map(function ($item) {
+    $collection = Student::query()->pluck('date_of_birth')->all();
+
+    $collection = collect($collection)->map(function ($item) {
         return Carbon::parse($item)->diff(Carbon::now())->y;
     });
+
     return view('pages.student_date',['average' => $collection->avg()]);
 });
 
 Route::get('count-country', function () {
-    $collection = Country::withCount('students')->get();
-    return view('pages.count_country',['collection' => $collection]);
+   $collection = Country::withCount('students')->pluck('students_count', 'name')->all();
+
+    return view('pages.count_country',['collection' => getCollection($collection)]);
 });
 
 Route::get('count-level', function () {
-    $collection = Level::withCount('students')->get();
-    return view('pages.count_level',['collection' => $collection]);
+    $collection = Level::withCount('students')->pluck('students_count', 'name')->all();
+
+    return view('pages.count_level',['collection' => getCollection($collection)]);
 });
+
+
+function getCollection($array): Collection
+{
+    return collect($array)->map(function ($key, $item) {
+        return [
+            'name' => $item,
+            'count' => $key
+        ];
+    })->values();
+}
